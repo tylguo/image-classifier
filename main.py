ce23128775f71188cda6815e7fa9879fefbe7c80
Tyler Guo <tyleguo2004@gmail.com>
2024-09-28 16:16:35 -0700

    update main.py

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ import streamlit as st
 from PIL import Image
 
 import tensorflow as tf
-from tensorflow.python.keras.datasets import cifar10
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Flatten, Dense
-from tensorflow.python.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.utils import to_categorical
 
-(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+(X_train, y_train), (X_val, y_val) = tf.keras.datasets.cifar10.load_data()
+X_train = X_train / 255
+X_val = X_val / 255
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_val, 10)
+
+model = Sequential([
+    Flatten(input_shape=(32, 32, 3)),
+    Dense(1000, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metric=['accuracy'])
+model.fit(X_train, y_train, batch_size=64, epochs=10, validation_data=(X_val, y_val))
+model.save('cifar10_model.h5')
