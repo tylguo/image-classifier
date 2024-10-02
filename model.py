0c5538c3c0069423169f473998bf89631e5c1287
Tyler Guo <tyleguo2004@gmail.com>
2024-10-01 17:25:45 -0700

    condense imports

--- model.py
+++ model.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-
 from PIL import Image
-
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout
